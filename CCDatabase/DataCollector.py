631a0d6b3de3c1762c3e6d6f1a8e102b8b59bfa0
NicoRicardi <niccolo.ricardi@unige.ch>
2020-09-15 13:40:35 +0200

    partial work on datacollector

--- CCDatabase/DataCollector.py
+++ CCDatabase/DataCollector.py
@@ -42,16 +42,17 @@ class DataCollector():
             self.parser_file = parser_file
             self.parserargsdict = parserargsdict
             self.parserkwargsdict = parserkwargsdict
+            # TODO: process
         else:   # deduce other args
             self.reqsdict = {cq: megadict[parsername]["reqs"][cq] for cq in cqlist}
             self.funcdict = megadict[parsername]["funcdict"]
             self.all_raws = [r[1] if type(r) in [tuple,list] else r for r in set(ittl.chain.from_iterable(reqsdict.values()))]
-            self.parserdict = {rq : megadict[parsername]["parser"] for rq in self.all_raws}
+            self.parserdict = {rq : megadict[parsername]["parser"] for rq in self.all_raws}  #TODO: change
             self.parser_file = megadict[parsername]["parser_file"]
 #            self.parserargsdict = {rq: megadict[parsername]["parserargs"][rq] for rq in self.all_raws}
             ### {rq: [{rq:hook,..}]} because parser should parse all_raws at once
-            self.parserargsdict = {rq:[{rq: megadict[parsername]["parser_args"][rq] for rq in self.all_raws}] for rq in self.all_raws}
-            self.parserkwargsdict = {rq: megadict[parsername]["parser_kwargs"] for rq in self.all_raws}
+            self.parserargsdict = {parsername: [{rq: megadict[parsername]["parser_args"][rq] for rq in self.all_raws}]}
+            self.parserkwargsdict = {parsername: megadict[parsername]["parser_kwargs"]}
         self.cqlist = cqlist 
         self.ex_qs = ex_qs if ex_qs else [cq for cq in cqlist if cq in megadict["ex_qs"]]
         self.ext = ext
@@ -99,20 +100,18 @@ class DataCollector():
         self.write_df(filename=filename,overwrite=overwrite)
         
         
-megadict = {"ccp": ccpdict, "qcepccp": qcepccpdict, "qcep": qcepdict, "ex_qs": ["ex_en"]}
-
 ccpdict = {"reqs": {"ex_en": "exc_energy_rel"}, 
            "funcdict": "ccp",
            "parser_file": "CCParser.json",
-           "parser": None,
-           "parser_args": {"exc_energy_rel":None},
+           "parser": "ccp",
+           "parser_args": {None},
            "parser_kwargs": {"exc_energy_rel":None}
            }
 
 qcepccpdict = {"reqs": {"ex_en": "exc_en"}, 
            "funcdict": "qcepccp",
            "parser_file": "CCParser.json",
-           "parser": qcep.parse_qchem,
+           "parser": "qcep",
            "parser_args": {"exc_energy": qcep.hooks["exc_energies"], "SCF": qcep.hooks["scf_energy"]},
            "parser_kwargs": {"json_file": "CCParser.json", "overwrite_vals":True}
            }
@@ -120,7 +119,9 @@ qcepccpdict = {"reqs": {"ex_en": "exc_en"},
 qcepdict = {"reqs": {"ex_en": "exc_en"}, 
            "funcdict": "qcep",
            "parser_file": "qcep.json",
-           "parser": qcep.parse_qchem,
+           "parser":"qcep",
            "parser_args": {"exc_energy": qcep.hooks["exc_energies"], "SCF": qcep.hooks["scf_energy"]},
            "parser_kwargs": {"json_file": "qcep.json", "overwrite_vals":True}
            }
+
+megadict = {"ccp": ccpdict, "qcepccp": qcepccpdict, "qcep": qcepdict, "ex_qs": ["ex_en"]}
